ea9a9a6d042e1057d3ac1847bc5b735cf1e06899
Ryan Geyer <rgeyer@its.jnj.com>
2010-12-30 10:47:36 -0800

    Debug stuff for now

---
+++ cookbooks/ruby_gems/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Ryan J. Geyer"
+maintainer_email "me@ryangeyer.com"
+license          "All rights reserved"
+description      "Installs/Configures ruby_gems"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "ruby_gems::default","Just testing for now"
---
+++ cookbooks/ruby_gems/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: ruby_gems
+# Recipe:: default
+#
+# Copyright 2010, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+Chef::Log.info("Hey look we're running on a #{node[:platform]}")
