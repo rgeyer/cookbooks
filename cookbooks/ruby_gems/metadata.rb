e1dc6ae13fd036ba9d35053d2121712b0b82f53f
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-24 11:30:08 -0800

    Hmmn.. I think adding the depends will help..

--- cookbooks/ruby_gems/metadata.rb
+++ cookbooks/ruby_gems/metadata.rb
@@ -5,12 +5,8 @@ description      "Installs/Configures ruby_gems"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-# I'm hesitant to actually include depends statements here, since the dependencies will differ based on the platform
-# and which RepoPath this repo/cookbook is a part of
-# depends "rubygems"
-# depends "rs_sandbox"
-
 depends "rubygems"
+depends "rs_sandbox"
 
 provides "ruby_gems[package]"
 
