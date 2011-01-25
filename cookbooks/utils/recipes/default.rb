36505dc8462cfda6dcb85d6bdfac699fe22a31a0
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-24 16:34:44 -0800

    Purely experimental

--- cookbooks/utils/recipes/default.rb
+++ cookbooks/utils/recipes/default.rb
@@ -7,9 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-`LocateSandbox.bat`
-Chef::Log.info(`echo %RS_SANDBOX_HOME%`)
-`SET PATH=%RS_SANDBOX_HOME%\Ruby\bin;%PATH%`
-Chef::Log.info(`echo %PATH%`)
+gem_path=`echo %RS_SANDBOX_HOME%`
+gem_path+="\Ruby\bin\gem"
 
-Chef::Log.info(`LocateSandbox && SET PATH=%RS_SANDBOX_PATH%\Ruby\bin;%PATH% && gem list`)
+Chef::Log.info(`#{gem_path} list`)
