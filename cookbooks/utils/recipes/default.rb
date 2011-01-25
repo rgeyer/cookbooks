c3bdffa2a27193e2a5983e8299d9f1f70d2fd5c8
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-24 16:53:28 -0800

    Purely experimental

--- cookbooks/utils/recipes/default.rb
+++ cookbooks/utils/recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+`SETX PATH=%RS_SANDBOX_HOME%\Ruby\bin;%PATH%;` unless `echo %RS_SANDBOX_HOME%` =~ /Ruby\\bin/
+
 gem_path=`echo %RS_SANDBOX_HOME%`.strip
 gem_path+="\\Ruby\\bin\\gem.bat"
 
