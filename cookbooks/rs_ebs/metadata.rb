a8c20f11751bfe861000e8fe09ad14c1badfd2ec
Ryan Geyer <rgeyer@its.jnj.com>
2011-02-02 12:07:52 -0800

    Added rs_ebs for the first time

---
+++ cookbooks/rs_ebs/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Ryan J. Geyer"
+maintainer_email "me@ryangeyer.com"
+license          "All rights reserved"
+description      "Installs/Configures rs_ebs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
