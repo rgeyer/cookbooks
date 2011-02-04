f21da3f5b6392acd4d6a136362428cb359585c2a
Ryan Geyer <rgeyer@its.jnj.com>
2011-02-04 10:06:44 -0800

    The install rest connection recipe is practically just a straight ruby script now, doesn't really do anything chefy..

--- cookbooks/utils/recipes/install_rest_connection_gem.rb
+++ cookbooks/utils/recipes/install_rest_connection_gem.rb
@@ -6,19 +6,24 @@ rest_connection_version="0.0.15"
 load_ruby_gem_into_rs_sandbox("i18n", nil, nil, true)
 load_ruby_gem_into_rs_sandbox("rest_connection", rest_connection_version, nil, false)
 
-directory value_for_platform("windows" => {"default" => "C:/Users/RightScale_1/.rest_connection"}, "default" => "/etc/rest_connection") do
+d = directory value_for_platform("windows" => {"default" => "C:/Users/RightScale_1/.rest_connection"}, "default" => "/etc/rest_connection") do
   recursive true
-  action :create
+  action :nothing
 end
 
-template value_for_platform("windows" => {"default" => "C:/Users/RightScale_1/.rest_connection/rest_api_config.yaml"}, "default" => "/etc/rest_connection/rest_api_config.yaml") do
+d.run_action(:create)
+
+t = template value_for_platform("windows" => {"default" => "C:/Users/RightScale_1/.rest_connection/rest_api_config.yaml"}, "default" => "/etc/rest_connection/rest_api_config.yaml") do
   source "rest_api_config.yaml.erb"
   variables(
     :rest_pass => node[:utils][:rest_pass],
     :rest_user => node[:utils][:rest_user],
     :rest_acct_num => node[:utils][:rest_acct_num]
   )
+  action :nothing
 end
+
+t.run_action(:create)
 
 # A useful way to find "this" instance when running scripts that use rest_connection
 # Tag.search('ec2_instance', ["ipv4:private=#{IPSocket.getaddress(Socket.gethostname)}"])
