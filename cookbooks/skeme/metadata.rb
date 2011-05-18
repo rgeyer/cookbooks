3b8966641e22d2feb69d1ca94bfa123505021a71
Ryan Geyer <me@ryangeyer.com>
2011-05-18 13:52:22 -0700

    Version bump for skeme fix

--- cookbooks/skeme/metadata.rb
+++ cookbooks/skeme/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "me@ryangeyer.com"
 license          "All rights reserved"
 description      "Installs/Configures skeme"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "rjg_aws"
 
