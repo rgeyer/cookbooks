d2f06ef6ed4c2382eca5481b63249d91701e4e63
Ryan Geyer <me@ryangeyer.com>
2011-05-18 11:11:29 -0700

    Opscode platform support for linux only

--- cookbooks/rjg_aws/recipes/default.rb
+++ cookbooks/rjg_aws/recipes/default.rb
@@ -12,19 +12,21 @@ include_recipe "rs_sandbox::default"
 gem_name = "right_aws"
 gem_version = "2.0.0"
 
-load_ruby_gem_into_rs_sandbox(gem_name, gem_version, nil, true)
-
+if node[:chef_packages][:chef][:chef_root].start_with? '/opt/rightscale/sandbox'
+  load_ruby_gem_into_rs_sandbox(gem_name, gem_version, nil, true)
+else
 # TODO: This currently only supports *nix OS's because there is no "system" ruby for windows.
-if node[:platform] != "windows"
-g = gem_package gem_name do
-    if gem_version
-      version gem_version
+  if node[:platform] != "windows"
+    g = gem_package gem_name do
+      if gem_version
+        version gem_version
+      end
+      action :nothing
     end
-    action :nothing
+
+    g.run_action(:install)
+
+    Gem.clear_paths
+    require gem_name
   end
-
-  g.run_action(:install)
-
-  Gem.clear_paths
-  require gem_name
 end
