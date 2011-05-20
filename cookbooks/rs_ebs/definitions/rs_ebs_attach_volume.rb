74a83f04937e8e9b2018ccaf57d0c05bd71a8d6f
Ryan Geyer <me@ryangeyer.com>
2011-05-19 22:59:35 -0700

    Automatic selection of device in absence of a device input

---
+++ cookbooks/rs_ebs/definitions/rs_ebs_attach_volume.rb
@@ -0,0 +1,165 @@
+#  Copyright 2011 Ryan J. Geyer
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# TODO: indempotency for windows
+
+define :rs_ebs_attach_volume,
+    :volume_name => nil,
+    :aws_access_key_id => nil,
+    :aws_secret_access_key => nil,
+    :device => nil,
+    :vol_size_in_gb => nil,
+    :snapshot_id => nil,
+    :mountpoint => nil,
+    :timeout => 300 do
+
+  include_recipe "rs_ebs::default"
+
+  # Capture the "before" list of drives and volumes in windows, so we can wait for the new drive to be added,
+  # and know which volume to assign the drive letter to
+  if node[:platform] == "windows"
+    powershell "Get current disk and volume list" do
+      ps_code = <<-EOF
+$drives = Get-WMIObject Win32_DiskDrive
+$device_ids = @()
+foreach($drive in $drives)
+{
+  $device_ids += $drive.DeviceID
+}
+Set-ChefNode rs_ebs_win32_disks -ArrayValue $device_ids
+
+$volumes = Get-WMIObject Win32_Volume
+$volume_ids = @()
+foreach($volume in $volumes)
+{
+  $volume_ids += $volume.DeviceID
+}
+Set-ChefNode rs_ebs_win32_volumes -ArrayValue $volume_ids
+      EOF
+    end
+
+    source(ps_code)
+  end
+
+  rjg_aws_ebs_volume params[:volume_name] do
+    aws_access_key params[:aws_access_key_id]
+    aws_secret_access_key params[:aws_secret_access_key]
+    device params[:device]
+    size params[:vol_size_in_gb].to_i
+    if params[:snapshot_id]
+      snapshot_id params[:snapshot_id]
+    end
+    action [:create, :attach]
+  end
+
+  if node[:platform] == "windows"
+    # TODO: This is a total hack, but if I don't do it, mounting later fails when I'm attaching
+    # a new volume from a snapshot.  Probably a symptom of another problem but this seems to fix it.
+    powershell "Wait for #{params[:device]}" do
+      parameters({'TIMEOUT' => params[:timeout]})
+      ps_code = <<-EOF
+      $drive_list = Get-ChefNode rs_ebs_win32_disks
+      $start_ts = [DateTime]::Now
+      do
+        Start-Sleep -s 2
+      while (($drive_list.count -eq $drive_count) -and (([DateTime]::Now - $start_ts) -lt $env:TIMEOUT))
+      EOF
+
+      source(ps_code)
+    end
+
+    powershell "Online, initialize, and format the drive" do
+      parameters({"MOUNTPOINT" => params[:mountpoint]})
+
+      ps_code = <<-EOF
+$drive_list = Get-ChefNode rs_ebs_win32_disks
+$volume_list = Get-ChefNoe rs_ebs_win32_volumes
+
+$drives = Get-WMIObject Win32_DiskDrive
+$device_ids = @()
+foreach($drive in $drives)
+{
+  if($drive_list -notcontains $drive.DeviceID)
+  {
+    $device_ids += $drive.DeviceID
+  }
+}
+
+$volumes = Get-WMIObject Win32_Volume
+$volume_ids = @()
+foreach($volume in $volumes)
+{
+  if($volume_list -notcontains $volume.DeviceID)
+  {
+    $volume_ids += $volume.DeviceID
+  }
+}
+
+$letters = 68..89 | ForEach-Object { ([char]$_)+":" }
+$freeletters = $letters | Where-Object {
+  (New-Object System.IO.DriveInfo($_)).DriveType -eq 'NoRootDirectory'
+}
+
+$letter = $env:MOUNTPOINT
+if (!$letter)
+{
+  $letter = $freeletters[0]
+}
+
+$filter = 'DeviceID="'+$device_ids[0].replace("\", "\\")+'"'
+$drive = Get-WMIObject Win32_DiskDrive -filter $filter
+$script = $Null
+
+if (($drive) -and ($drive.Partitions -eq "0")) {
+  $drivenumber = $drive.DeviceID -replace '[\\\\\.\\physicaldrive]',''
+  $script = @"
+select disk $drivenumber
+online disk noerr
+attributes disk clear readonly noerr
+create partition primary noerr
+format quick
+"@
+
+  $script | diskpart
+}
+
+mountvol $freeletters[0] $volume_ids[0]
+      EOF
+      source(ps_code)
+    end
+  else
+    # TODO: This is a total hack, but if I don't do it, mounting later fails when I'm attaching
+    # a new volume from a snapshot.  Probably a symptom of another problem but this seems to fix it.
+    ruby_block "Wait for #{params[:device]}" do
+      block do
+        start_ts = Time.now.to_i
+        while !::File.exist?('/dev/sdi') && (Time.now.to_i - start_ts) < params[:timeout] do
+          sleep(2)
+        end
+      end
+    end
+
+    bash "Format #{params[:device]} with XFS" do
+      user "root"
+      code <<-EOF
+    grep -q xfs /proc/filesystems || modprobe xfs
+    mkfs.xfs -q #{params[:device]}
+      EOF
+      not_if do
+        params[:snapshot_id] or
+        `file -s #{params[:device]} | grep XFS`.strip =~ /XFS/
+      end
+    end
+  end
+
+end
