d255b43df42ee91558f8e2aadb6c08edac29549c
Ryan Geyer <me@ryangeyer.com>
2011-05-24 09:57:51 -0700

    Install rest connection in RS sandbox conditionally. Store creds conditionally.

--- cookbooks/utils/recipes/install_rest_connection_gem.rb
+++ cookbooks/utils/recipes/install_rest_connection_gem.rb
@@ -3,27 +3,47 @@ include_recipe "rs_sandbox::default"
 require 'socket'
 rest_connection_version="0.0.15"
 
-load_ruby_gem_into_rs_sandbox("i18n", nil, nil, true)
-load_ruby_gem_into_rs_sandbox("rest_connection", rest_connection_version, nil, false)
-
-d = directory value_for_platform("windows" => {"default" => "#{node[:rs_sandbox][:rl_user_home_dir]}/.rest_connection"}, "default" => "/etc/rest_connection") do
-  recursive true
-  action :nothing
+# Install rest_connection in the RightScale sandbox, if it exists.
+if ::File.directory? node[:rs_sandbox][:home]
+  load_ruby_gem_into_rs_sandbox("i18n", nil, nil, true)
+  load_ruby_gem_into_rs_sandbox("rest_connection", rest_connection_version, nil, false)
 end
 
-d.run_action(:create)
+# Install rest_connection for the system, if we're on linux
+# TODO: This currently only supports *nix OS's because there is no "system" ruby for windows.
+if node[:platform] != "windows"
+  g = gem_package "rest_connection" do
+    version rest_connection_version
+    action :nothing
+  end
 
-t = template value_for_platform("windows" => {"default" => "#{node[:rs_sandbox][:rl_user_home_dir]}/.rest_connection/rest_api_config.yaml"}, "default" => "/etc/rest_connection/rest_api_config.yaml") do
-  source "rest_api_config.yaml.erb"
-  variables(
-    :rest_pass => node[:utils][:rest_pass],
-    :rest_user => node[:utils][:rest_user],
-    :rest_acct_num => node[:utils][:rest_acct_num]
-  )
-  action :nothing
+  g.run_action(:install)
+
+  Gem.clear_paths
+  require gem_name
 end
 
-t.run_action(:create)
+# If credentials were provided, create the settings/credentials file
+if node[:utils] && node[:utils][:rest_pass] && node[:utils][:rest_user] && node[:utils][:rest_acct_num]
+  d = directory value_for_platform("windows" => {"default" => "#{node[:rs_sandbox][:rl_user_home_dir]}/.rest_connection"}, "default" => "/etc/rest_connection") do
+    recursive true
+    action :nothing
+  end
+
+  d.run_action(:create)
+
+  t = template value_for_platform("windows" => {"default" => "#{node[:rs_sandbox][:rl_user_home_dir]}/.rest_connection/rest_api_config.yaml"}, "default" => "/etc/rest_connection/rest_api_config.yaml") do
+    source "rest_api_config.yaml.erb"
+    variables(
+      :rest_pass => node[:utils][:rest_pass],
+      :rest_user => node[:utils][:rest_user],
+      :rest_acct_num => node[:utils][:rest_acct_num]
+    )
+    action :nothing
+  end
+
+  t.run_action(:create)
+end
 
 # A useful way to find "this" instance when running scripts that use rest_connection
 # Tag.search('ec2_instance', ["ipv4:private=#{IPSocket.getaddress(Socket.gethostname)}"])
