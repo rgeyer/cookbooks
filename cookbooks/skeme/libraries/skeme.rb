55938b32177b1ee540ff5208d361a513cb2ecfd9
Ryan Geyer <me@ryangeyer.com>
2011-05-26 13:27:40 -0700

    Can we install the gem in a library method before we use it?

--- cookbooks/skeme/libraries/skeme.rb
+++ cookbooks/skeme/libraries/skeme.rb
@@ -22,6 +22,27 @@ module Rgeyer
     module Skeme
 
       def skeme_gem
+        include_recipe "rs_sandbox::default"
+
+        # Install rest_connection in the RightScale sandbox, if it exists.
+        if ::File.directory? node[:rs_sandbox][:home]
+          load_ruby_gem_into_rs_sandbox("skeme", "0.0.3", nil, true)
+        end
+
+        # Install rest_connection for the system, if we're on linux
+        # TODO: This currently only supports *nix OS's because there is no "system" ruby for windows.
+        if node[:platform] != "windows"
+          g = gem_package "skeme" do
+            version "0.0.3"
+            action :nothing
+          end
+
+          g.run_action(:install)
+
+          Gem.clear_paths
+          require "skeme"
+        end
+
         @@skeme_gem ||= ::Skeme::Skeme.new({
           :aws_access_key_id => new_resource.aws_access_key,
           :aws_secret_access_key => new_resource.aws_secret_access_key,
