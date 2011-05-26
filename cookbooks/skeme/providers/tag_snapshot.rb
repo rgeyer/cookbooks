9f2298c8fc85dd4c624e36faa660b54bbe828a20
Ryan Geyer <me@ryangeyer.com>
2011-05-26 13:18:01 -0700

    Can we include a recipe from a provider

--- cookbooks/skeme/providers/tag_snapshot.rb
+++ cookbooks/skeme/providers/tag_snapshot.rb
@@ -11,6 +11,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+include_recipe "skeme::default"
+
 include Rgeyer::Chef::Skeme
 
 action :add do
