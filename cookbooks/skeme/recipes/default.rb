764d8786c6735f64c94cdb2786979d9cc92cf457
Ryan Geyer <me@ryangeyer.com>
2011-05-26 12:15:06 -0700

    Newer skeme version that's compatible with other cookbooks that require right_aws 2.0.0

--- cookbooks/skeme/recipes/default.rb
+++ cookbooks/skeme/recipes/default.rb
@@ -21,14 +21,14 @@ require 'socket'
 
 # Install rest_connection in the RightScale sandbox, if it exists.
 if ::File.directory? node[:rs_sandbox][:home]
-  load_ruby_gem_into_rs_sandbox("skeme", "0.0.1", nil, true)
+  load_ruby_gem_into_rs_sandbox("skeme", "0.0.2", nil, true)
 end
 
 # Install rest_connection for the system, if we're on linux
 # TODO: This currently only supports *nix OS's because there is no "system" ruby for windows.
 if node[:platform] != "windows"
   g = gem_package "skeme" do
-    version "0.0.1"
+    version "0.0.2"
     action :nothing
   end
 
