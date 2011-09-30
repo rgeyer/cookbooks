2844a9e7e3b03e866fd195c7905f9330ae709448
Ryan J. Geyer <me@ryangeyer.com>
2011-09-30 11:19:15 -0700

    Credit where credit is due

--- cookbooks/databags/libraries/databags.rb
+++ cookbooks/databags/libraries/databags.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# inspired by https://gist.github.com/867960
+
 require 'json'
 
 if Chef::Config[:solo]
