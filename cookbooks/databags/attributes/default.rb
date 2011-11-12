9c11828ce39c9c95e0d692992d388b96e7b5c186
Ryan J. Geyer <me@ryangeyer.com>
2011-11-11 16:16:19 -0800

    databags default attributes

---
+++ cookbooks/databags/attributes/default.rb
@@ -0,0 +1,21 @@
+default[:databags][:users] = <<EOF
+[
+  {
+    "id":"rgeyer",
+    "groups" : ["znc"],
+    "znc":{
+      "real_name":"Ryan J. Geyer",
+      "buffer":"500",
+      "ident":"rgeyer",
+      "server":"irc.freenode.net",
+      "pass":"sha256#b3a012e0e2c70b88ef166a11246ab2b43ce579730cf50f4354e8ef6c187c6760#F77iWUY-AJ1iHy..Zy4G#",
+      "channels":[
+        {
+          "#rightscale":""
+        }
+      ],
+      "modules": []
+    }
+  }
+]
+EOF
